8b3ad113be0238599f2a40c95a81146b8499b47d
Sagar Medtiya <sagarmedtiya@gmail.com>
2025-07-09 01:48:10 +0530

    init

---
+++ src/test/java/com/example/Bitespeed/Customer/Identity/Tracking/Solution/BitespeedCustomerIdentityTrackingSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bitespeed.Customer.Identity.Tracking.Solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitespeedCustomerIdentityTrackingSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
