d8099760cefd18d247c53585741c0f485ccad4bc
Sagar Medtiya <sagarmedtiya@gmail.com>
2025-07-08 22:15:26 +0530

    add the repository file

---
+++ src/main/java/com/example/repository/ContactRepository.java
@@ -0,0 +1,22 @@
+package com.example.repository;
+
+import com.example.entity.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ContactRepository  extends JpaRepository<Contact, Integer> {
+    @Query("SELECT c FROM Contact c WHERE "+
+    "(c.email = :email OR :email IS NULL) OR "+
+    "(c.phoneNumber = :phoneNumber OR :phoneNumber IS NULL)" +
+    "AND c.deletedAt IS NULL")
+    List<Contact> findByEmailOrPhoneNumber(@Param("email") String email,
+                                           @Param("phoneNumber") String phoneNumber);
+
+    @Query("SELECT c FROM Contact c WHERE " +
+    "c.linkedId = :linkedId OR c.id = :linkedId "+
+    "AND c.deletedAt IS NULL")
+    List<Contact> findByLinkedIdOrId(Integer id, Integer id1);
+}
