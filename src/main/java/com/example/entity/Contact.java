98e6333016a7585f88fb664d1f63b465bdf32083
Sagar Medtiya <sagarmedtiya@gmail.com>
2025-07-08 22:16:08 +0530

    add the entity file

--- src/main/java/com/example/entity/Contact.java
+++ src/main/java/com/example/entity/Contact.java
@@ -1,4 +1,41 @@
 package com.example.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="contact")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String phoneNumber;
+    private String email;
+
+    @Column(name="linked_id")
+    private Integer linkedId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="link_precedence")
+    private LinkPrecedence linkPrecedence;
+
+    @Column(name="created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name="deleted_at")
+    private LocalDateTime deletedAt;
+
 }
+
